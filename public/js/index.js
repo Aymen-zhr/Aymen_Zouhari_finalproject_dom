9be2012555fd8ae5e1f18dc22205e6641081a11d
Aymen-zhr <aymenzouhari066@gmail.com>
2026-01-19 15:28:44 +0100

    feat: add responsive to navbar and made it functional

--- public/js/index.js
+++ public/js/index.js
@@ -5,3 +5,11 @@ dropdown.addEventListener('click', function(){
     dropdownContent.classList.toggle('show');
 });
 
+let nav = document.querySelector('.nav-list');
+document.querySelector('.fa-bars').addEventListener('click', function(){
+    nav.classList.add('show');
+});
+
+document.querySelector('.nav-list-span').addEventListener('click', function(){
+    nav.classList.remove('show');
+});
