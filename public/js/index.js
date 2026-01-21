18c8280b498f5eadcb0f8027afbc16d5242c3179
Aymen-zhr <aymenzouhari066@gmail.com>
2026-01-21 11:39:09 +0100

    feat: add menu with its style

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,15 @@
 let dropdown = document.querySelector('.dropdown');
 let dropdownContent = document.querySelector('.dropdown-content');
 
-dropdown.addEventListener('click', function(){
+dropdown.addEventListener('click', function () {
     dropdownContent.classList.toggle('show');
 });
 
 let nav = document.querySelector('.nav-list');
-document.querySelector('.fa-bars').addEventListener('click', function(){
+document.querySelector('.fa-bars').addEventListener('click', function () {
     nav.classList.add('show');
 });
 
-document.querySelector('.nav-list-span').addEventListener('click', function(){
+document.querySelector('.nav-list-span').addEventListener('click', function () {
     nav.classList.remove('show');
 });
