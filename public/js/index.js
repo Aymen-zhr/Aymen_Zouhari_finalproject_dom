c439216c2fbe4e998d21bc17fb08d37f9cc2122c
Aymen-zhr <aymenzouhari066@gmail.com>
2026-01-21 12:31:49 +0100

    feat: added a testimonials with its style

--- public/js/index.js
+++ public/js/index.js
@@ -13,3 +13,26 @@ document.querySelector('.fa-bars').addEventListener('click', function () {
 document.querySelector('.nav-list-span').addEventListener('click', function () {
     nav.classList.remove('show');
 });
+
+const testimonialItems = document.querySelectorAll('.testimonial-item');
+const dots = document.querySelectorAll('.dot');
+let currentSlide = 0;
+
+function showSlide(index) {
+    testimonialItems.forEach(item => item.classList.remove('active'));
+    dots.forEach(dot => dot.classList.remove('active'));
+    testimonialItems[index].classList.add('active');
+    dots[index].classList.add('active');
+}
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        currentSlide = index;
+        showSlide(currentSlide);
+    });
+});
+
+setInterval(() => {
+    currentSlide = (currentSlide + 1) % testimonialItems.length;
+    showSlide(currentSlide);
+}, 5000);
